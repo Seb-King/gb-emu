14b3522af8ac926eba6f05aae16c97f781d6c2b3
Seb King <sebastian.x.king@gmail.com>
2023-04-09 13:04:12 +1000

    include paths for sdl headers for windows

--- src/file_handling.cpp
+++ src/file_handling.cpp
@@ -2,11 +2,17 @@
 #include <vector>
 #include <fstream>
 #include "typedefs.hpp"
+#include <filesystem>
+#include <iostream>
 
 std::vector<u8> readFile(std::string fileName) {
+	if(!std::filesystem::exists(fileName)) {
+		std::cout << "File not found!" << std::endl;
+		throw std::runtime_error("File not found!");
+	}
 	std::ifstream file(fileName, std::ios::binary);
 
-	std::vector<u8> buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>(file));
+	std::vector<u8> buffer((std::istreambuf_iterator<char>(file)), {});
 
 	return buffer;
 }
