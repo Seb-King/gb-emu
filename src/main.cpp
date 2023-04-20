7b120a55220bfccbde1b3f77221f637c88099946
Seb King <sebastian.king@ailo.io>
2023-04-20 22:49:51 +1000

    parse command line flags

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,37 @@
 #include "game.hpp"
 
-std::string getRomPath() {
-	return "/Users/sebking/dev/gb-emu/roms/dr-mario.gb"; 
-}
-
 int main(int argc, char* argv[]) {
 	RunOptions options;
-	options.LOG_STATE = false;
-	options.NO_DISPLAY = false;
-	game_loop(getRomPath(), options);
+	options.LOG_STATE = true;
+	options.NO_DISPLAY = true;
+
+	std::string rom = "";
+
+	for (int i = 1; i < argc; i++) {
+		std::string arg = argv[i];
+
+		if (arg == "-h" || arg == "--help") {
+				std::cout << "Usage: " << argv[0] << " [options]\n";
+				std::cout << "Options:\n";
+				std::cout << "  -h, --help        Display this help message\n";
+				std::cout << "  -r, --rom         Path to rom\n";
+				return 0;
+		}
+
+		if (arg == "-r" || arg == "--rom") {
+				if (i + 1 < argc) {
+						rom = argv[i + 1];
+						i++;
+				} else {
+						std::cerr << "Error: missing argument for " << arg << "\n";
+						return 1;
+				}
+		}
+	}
+
+	if (rom == "") {
+		exit(1);
+	}
+
+	game_loop(rom, options);
 }
