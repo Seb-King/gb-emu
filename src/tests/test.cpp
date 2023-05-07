f60781d6e711726091b6e1f8c0a751aca2dab1bf
Seb King <sebastian.king@ailo.io>
2023-05-07 17:24:30 +1000

    Add emulator core target and link against main and test executables
    this allows the emulator functions to be included in tests without re-compilation

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
+#include "../utils.hpp"
 
 TEST(HelloTest, TestAssertions) {
+  println("henlo");
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
