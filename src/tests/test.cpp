ce06a58313a7d49408a40d9e715c7a3e790e47a2
Seb King <sebastian.king@ailo.io>
2023-05-07 11:01:20 +1000

    move tests to src/
    this resolves an issue with vscode cmake tools not being able to find gtest/gtest.h

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
+TEST(HelloTest, TestAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
