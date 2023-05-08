69a043a4ba2bd3e3578d9989289d978388c42fde
Seb King <sebastian.king@ailo.io>
2023-05-08 21:08:41 +1000

    add NoMBC cartridge implementation

--- src/cartridge.cpp
+++ src/cartridge.cpp
@@ -11,8 +11,17 @@ u8 Cartridge::read(u16 addr) {
 
 void Cartridge::write(u8 val, u16 addr) {}
 
+class NoMemoryBankControllerCartridge : public Cartridge {
+  vector<u8> memory;
+public:
+  void write(u8 val, u16 addr) {
+    if (addr >= 0xA000 && addr <= 0xBFFF)
+      memory.at(addr) = val;
+  }
+};
+
 Cartridge* cart_factory(std::vector<u8> game_data) {
-  Cartridge* cart = new Cartridge();
+  Cartridge* cart = new NoMemoryBankControllerCartridge();
   cart->init(game_data);
   return cart;
 }
