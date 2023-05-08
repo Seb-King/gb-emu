84663a58d61264260224707fbd6a86ded98cfcd2
Seb King <sebastian.king@ailo.io>
2023-05-08 21:59:58 +1000

    add some tests for NoMBC implementation

--- src/tests/cart_test.cpp
+++ src/tests/cart_test.cpp
@@ -10,10 +10,16 @@ TEST(Cart_Factory_Tests, ASSERTIONS) {
   EXPECT_TRUE(cart->cart_type() == "NoMBC");
 }
 
-TEST(Cart_Tests, READ_ASSERTIONS) {
-  std::vector<u8> memory(0x8000, 1);
+TEST(NoMBC_Cart_Tests, READ_ASSERTIONS) {
+  std::vector<u8> memory(0xC000, 1);
   memory.at(0x0147) = 0x00;
   Cartridge* cart = cart_factory(memory);
 
-  EXPECT_EQ(cart->read(0x0001), 0x0001);
+  EXPECT_EQ(cart->read(0x0001), 0x01);
+
+  cart->write(0x12, 0x0333);
+  EXPECT_EQ(cart->read(0x0333), 0x01);
+
+  cart->write(0x10, 0xA000);
+  EXPECT_EQ(cart->read(0xA000), 0x10);
 }
