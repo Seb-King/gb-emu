9086131c3b1ec23c21db31224867c1f224e9ea2a
Seb King <sebastian.king@ailo.io>
2023-05-10 09:14:12 +1000

    model RAM chip on cartridge

--- src/cartridge.cpp
+++ src/cartridge.cpp
@@ -5,23 +5,39 @@
 using namespace std;
 
 class NoMemoryBankControllerCartridge : public Cartridge {
-  vector<u8> memory;
+  vector<u8> ROM;
+  vector<u8> RAM;
 public:
   string cart_type() {
     return "NoMBC";
   };
 
-  void init(std::vector<u8> mem) {
-    memory = mem;
+  void init(vector<u8> game_data) {
+    ROM = game_data;
+    RAM = *(new vector<u8>(0x2000, 0));
   }
 
   void write(u8 val, u16 addr) {
     if (addr >= 0xA000 && addr <= 0xBFFF)
-      memory.at(addr) = val;
+      RAM.at(addr) = val;
+  }
+
+  u8 read_rom(u16 addr) {
+    return ROM.at(addr);
+  }
+
+  u8 read_ram(u16 addr) {
+    return RAM.at(addr - 0xA000);
   }
 
   u8 read(u16 addr) {
-    return memory.at(addr);
+    if (addr >= 0 && addr <= 0x7FFF) {
+      return read_rom(addr);
+    } else if (addr >= 0xA000 && addr <= 0xBFFF) {
+      return read_ram(addr);
+    }
+
+    return 0xFF;
   }
 };
 
